18b3b54cb053279d351c515347f16be6acdffe3a
吴梦 <wumeng@wumengdeMacBook-Air.local>
2020-06-18 22:26:04 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{name}}</h1>
+</template>
+<script>
+export default {
+    name: 'app',
+    data() {
+        return {
+            name: 'app'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
